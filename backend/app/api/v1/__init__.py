6d7ec071a7592e9a1e2c31caff5bfc922a9da1be
lukas <lukas.garrido3967@gmail.com>
2026-09-17 20:28:38 -0300

    chore(backend): register auth router in api/v1

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -3,8 +3,10 @@ from app.api.v1 import services
 from app.api.v1 import clients
 from app.api.v1 import users
 from app.api.v1 import reservations
+from app.api.v1 import auth
 
 router = APIRouter()
+router.include_router(auth.router)
 router.include_router(services.router)
 router.include_router(clients.router)
 router.include_router(users.router)
